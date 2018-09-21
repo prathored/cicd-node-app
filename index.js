a1d0b019898371f29528263fdcd02736e1d23b0b
Priyanshu Rathore <priyanshu@mlabs.in>
2018-09-21 13:52:00 +0530

    simple node app with test

---
+++ index.js
@@ -0,0 +1,15 @@
+//importing node framework
+var express = require('express');
+
+var app = express();
+//Respond with "hello world" for requests that hit our root "/"
+app.get('/', function (req, res) {
+    res.send('hello world');
+    console.log('someone visited the site');
+});
+//listen to port 3000 by default
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is up and running on port 3000');
+});
+
+module.exports = app;
